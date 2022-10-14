97588954f3000ec522320a7fd096640137c3ef9c
mahmud035 <mahamudulhasan4148@gmail.com>
2022-10-14 07:02:29 +0600

    get value from form input field

--- src/App.js
+++ src/App.js
@@ -5,12 +5,25 @@ import { getAuth } from 'firebase/auth';
 const auth = getAuth(app);
 
 function App() {
+  const handleRegister = (e) => {
+    e.preventDefault();
+    console.log(e.target.email.value);
+    console.log(e.target.password.value);
+  };
+
   return (
     <div className="App">
-      <form>
-        <input type="email" name="" id="" placeholder=" Your email" />
+      <form onSubmit={handleRegister}>
+        <input type="email" name="email" id="" placeholder=" Your email" />
         <br />
-        <input type="password" name="" id="" placeholder=" Your password" />
+        <input
+          type="password"
+          name="password"
+          id=""
+          placeholder=" Your password"
+        />
+        <br />
+        <button type="submit">Register</button>
       </form>
     </div>
   );
