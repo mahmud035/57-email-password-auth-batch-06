0ad7ab6ed66a67ddcea56a57b97751302514fe59
mahmud035 <mahamudulhasan4148@gmail.com>
2022-10-14 06:37:36 +0600

    57-1 Login, Registration, Sign up, Sign in, project setup

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import { getAuth } from 'firebase/auth';
 const auth = getAuth(app);
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <form>
+        <input type="email" name="" id="" placeholder=" Your email" />
+        <br />
+        <input type="password" name="" id="" placeholder=" Your password" />
+      </form>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/_steps.js
@@ -0,0 +1,7 @@
+/* 
+//* Auth Terminology:
+1. "Register" or "Sign up" [First time create a user]
+2. "log in" or "sign in" [Already you have user account created] 
+
+
+*/
