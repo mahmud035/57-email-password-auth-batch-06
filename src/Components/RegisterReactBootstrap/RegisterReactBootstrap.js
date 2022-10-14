ef15627f332f595a238b1668578635b0f8d85676
mahmud035 <mahamudulhasan4148@gmail.com>
2022-10-14 10:34:02 +0600

    57-4 Create new user using email and password on firebase and basic form validation using react-bootstrap

--- src/Components/RegisterReactBootstrap/RegisterReactBootstrap.js
+++ src/Components/RegisterReactBootstrap/RegisterReactBootstrap.js
@@ -1,23 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './RegisterReactBootstrap.css';
+import app from '../../Firebase/firebase.init';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 import { Button, Form } from 'react-bootstrap';
 
+const auth = getAuth(app);
+
 const RegisterReactBootstrap = () => {
+  const [validated, setValidated] = useState(false);
+
   const handleRegister = (e) => {
-    e.preventDefault();
+    const form = e.currentTarget;
     const email = e.target.email.value;
     const password = e.target.password.value;
     console.log(email, password);
+
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
+    setValidated(true);
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error('error:', error);
+      });
   };
 
   return (
     <div className="form-container">
       <h3 className="text-white">Please Register</h3>
 
-      <Form onSubmit={handleRegister} className="w-50 mx-auto form">
+      <Form
+        onSubmit={handleRegister}
+        noValidate
+        validated={validated}
+        className="w-50 mx-auto form"
+      >
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label className="label">Email address</Form.Label>
-          <Form.Control type="email" name="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Please provide valid email.
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -26,11 +61,15 @@ const RegisterReactBootstrap = () => {
             type="password"
             name="password"
             placeholder="Password"
+            required
           />
+          <Form.Control.Feedback type="invalid">
+            Please provide valid password.
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Button variant="primary" type="submit">
-          Submit
+          Register
         </Button>
       </Form>
     </div>
