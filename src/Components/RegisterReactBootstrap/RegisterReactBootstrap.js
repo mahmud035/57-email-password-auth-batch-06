8f1dd67af205621a6637fa33690066b0e03c72d7
mahmud035 <mahamudulhasan4148@gmail.com>
2022-10-14 09:43:16 +0600

    57-3 Use Bootstrap Form and get user email and password

--- src/Components/RegisterReactBootstrap/RegisterReactBootstrap.js
+++ src/Components/RegisterReactBootstrap/RegisterReactBootstrap.js
@@ -3,18 +3,30 @@ import './RegisterReactBootstrap.css';
 import { Button, Form } from 'react-bootstrap';
 
 const RegisterReactBootstrap = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+  };
+
   return (
     <div className="form-container">
       <h3 className="text-white">Please Register</h3>
-      <Form className="w-50 mx-auto form">
+
+      <Form onSubmit={handleRegister} className="w-50 mx-auto form">
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label className="label">Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" name="email" placeholder="Enter email" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label className="label">Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
         </Form.Group>
 
         <Button variant="primary" type="submit">
